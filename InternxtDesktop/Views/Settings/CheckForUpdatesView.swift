106cecd6db6de9c3d6a06baf940a0cf3757bcbdd
patricioxavier8 <116120453+patricioxavier8@users.noreply.github.com>
2025-02-21 09:14:43 -0500

    testing purposes

--- InternxtDesktop/Views/Settings/CheckForUpdatesView.swift
+++ InternxtDesktop/Views/Settings/CheckForUpdatesView.swift
@@ -30,7 +30,33 @@ struct CheckForUpdatesView: View {
     }
     
     var body: some View {
-        AppButton(title: "SETTINGS_CHECK_FOR_UPDATES", onClick: updater.checkForUpdates, type: .secondary, size: .MD).disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+        AppButton(title: "SETTINGS_CHECK_FOR_UPDATES", onClick: askUserToDeleteApp, type: .secondary, size: .MD)//.disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+    
+    func askUserToDeleteApp() {
+        let openPanel = NSOpenPanel()
+        openPanel.title = "Selecciona la aplicación a eliminar"
+        openPanel.message = "Selecciona la aplicación que deseas eliminar"
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.allowedFileTypes = ["app"]
+
+        if openPanel.runModal() == .OK, let selectedApp = openPanel.url {
+            deleteApp(at: selectedApp.path)
+        } else {
+            print("El usuario canceló la selección.")
+        }
+    }
+    
+    func deleteApp(at path: String) {
+        let fileManager = FileManager.default
+
+        do {
+            try fileManager.removeItem(atPath: path)
+            print("Aplicación eliminada con éxito.")
+        } catch {
+            print("Error al eliminar la aplicación: \(error)")
+        }
     }
 }
 
